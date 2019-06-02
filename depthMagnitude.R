15b055f49abef3ed5cf12fe92c14d5430c61400d
Abdul Mohamed <abdulramenmohamed@gmail.com>
2019-06-02 14:33:54 -0700

    Plotted points on geom smooth

--- depthMagnitude.R
+++ depthMagnitude.R
@@ -5,8 +5,11 @@ quakeData <- read.csv("pnw-cat-recent-processed.csv", stringsAsFactors = FALSE)
 depthMagnitude <- function(earthquakes) {
   trimmedQuakes <-  earthquakes %>% select(lat:mag)
   depthMagCorrelate <- trimmedQuakes %>% ggplot() +
-                        geom_smooth(aes(mag, depth)) +
-                        theme_light()
+                        geom_point(aes(depth, mag), position = "jitter", color = "red", size = 1) +
+                        geom_smooth(aes(depth, mag), size = 2, color = "purple", level = .99) +
+                        theme_classic() +
+                        labs(x = "Depth (Metres)", y = "Magnitude (Richter)",
+                             title = "Depth vs. Magnitude")
   print(depthMagCorrelate)
 }
 
