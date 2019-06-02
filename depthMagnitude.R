51944ea784529971b6ed410f7c6634230f94de49
Abdul Mohamed <abdulramenmohamed@gmail.com>
2019-06-02 14:56:08 -0700

    Changed colors and added subtitles

--- depthMagnitude.R
+++ depthMagnitude.R
@@ -5,11 +5,13 @@ quakeData <- read.csv("pnw-cat-recent-processed.csv", stringsAsFactors = FALSE)
 depthMagnitude <- function(earthquakes) {
   trimmedQuakes <-  earthquakes %>% select(lat:mag)
   depthMagCorrelate <- trimmedQuakes %>% ggplot() +
-                        geom_point(aes(depth, mag), position = "jitter", color = "red", size = 1) +
-                        geom_smooth(aes(depth, mag), size = 2, color = "purple", level = .99) +
-                        theme_classic() +
+                        geom_point(aes(depth, mag), position = "jitter", color = "yellow", size = 1) +
+                        scale_color_continuous() +
+                        geom_smooth(aes(depth, mag), size = 1.5, color = "purple", level = .99) +
+                        theme_minimal() +
                         labs(x = "Depth (Metres)", y = "Magnitude (Richter)",
-                             title = "Depth vs. Magnitude")
+                             title = "Depth vs. Magnitude", subtitle = 
+                             "Shaded area represents a 99% Conidence Interval")
   print(depthMagCorrelate)
 }
 
